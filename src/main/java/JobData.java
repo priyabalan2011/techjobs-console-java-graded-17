9ef94d6d873d0def512d96922d5b49e4b80fc2d4
Balan Karuppasamy <agnibalan@gmail.com>
2023-08-29 17:55:41 -0500

    first commit

--- src/main/java/JobData.java
+++ src/main/java/JobData.java
@@ -5,10 +5,7 @@ import org.apache.commons.csv.CSVRecord;
 import java.io.FileReader;
 import java.io.IOException;
 import java.io.Reader;
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.HashMap;
-import java.util.List;
+import java.util.*;
 
 /**
  * Created by LaunchCode
@@ -75,7 +72,7 @@ public class JobData {
 
             String aValue = row.get(column);
 
-            if (aValue.contains(value)) {
+            if (aValue.toLowerCase().contains(value.toLowerCase())) {
                 jobs.add(row);
             }
         }
@@ -95,7 +92,22 @@ public class JobData {
         loadData();
 
         // TODO - implement this method
-        return null;
+        ArrayList<HashMap<String, String>> jobs = new ArrayList<>();
+
+        for (HashMap<String, String> row : allJobs) {
+
+            //  String aValue = row.get(column);
+            for (Map.Entry<String, String> sublist : row.entrySet()) {
+
+                if (sublist.getValue().toLowerCase().contains(value.toLowerCase())) {
+                    jobs.add(row);
+                    break;
+                }
+            }
+        }
+
+        return jobs;
+       // return null;
     }
 
     /**
--- src/main/java/TechJobs.java
+++ src/main/java/TechJobs.java
@@ -120,6 +120,22 @@ public class TechJobs {
     // Print a list of jobs
     private static void printJobs(ArrayList<HashMap<String, String>> someJobs) {
 
-        System.out.println("printJobs is not implemented yet");
+       // System.out.println("printJobs is not implemented yet");
+
+        if(someJobs.isEmpty()) {
+            System.out.print("No Results");
+        }else {
+            for (HashMap<String, String> someJob : someJobs) {
+
+                //System.out.println(someJobs.get(i));
+                System.out.println("\n*****");
+                for (Map.Entry<String, String> row : someJob.entrySet()) {
+                    System.out.println(row.getKey() + ": " + row.getValue());
+                }
+                System.out.println("*****");
+            }
+        }
+
+
     }
 }
